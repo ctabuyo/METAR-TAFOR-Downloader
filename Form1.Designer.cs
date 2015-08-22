3fbfeba0f1c486505105c439fa66b195f810e60d
Cristian Tabuyo <cristian@tabuyo.es>
2015-08-22 14:56:06 +0200

    sdfgdsf
    dgfdfgdf

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ICAO = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(198, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mostrar TAFOR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Introducir aeropuerto(ICAO):";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 58);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Mostrar METAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ICAO
+            // 
+            this.ICAO.Location = new System.Drawing.Point(235, 32);
+            this.ICAO.Name = "ICAO";
+            this.ICAO.Size = new System.Drawing.Size(130, 20);
+            this.ICAO.TabIndex = 3;
+            this.ICAO.TextChanged += new System.EventHandler(this.ICAO_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 216);
+            this.Controls.Add(this.ICAO);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "METAR/TAFOR Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox ICAO;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string code;
+
+            code = ICAO.Text;
+
+            using (WebClient client = new WebClient())
+            {
+                string s = client.DownloadString("http://wx.ivao.aero/taf.php?id="+code);
+                if (s == "") {
+                    s = ("No hay datos de TAFOR para este aeropuerto :-(");
+                }
+                MessageBox.Show(s);
+            }
+
+            
+        }
+
+        private void ICAO_TextChanged(object sender, EventArgs e)
+        {
+            ICAO.MaxLength = 4;
+
+            
+
+       }
+
+        private void button2_Click(object sender, EventArgs e)
+                        
+        {
+            string code;
+
+            code = ICAO.Text;
+
+            using (WebClient client = new WebClient())
+
+            {
+                string s = client.DownloadString("http://wx.ivao.aero/metar.php?id=" + code);
+                if (s == "")
+                {
+                    s = ("No hay datos de METAR para este aeropuerto :-(");
+                }
+                
+                MessageBox.Show(s);
+            }
+        }
+    }
+}
